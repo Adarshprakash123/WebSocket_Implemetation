2c9e7e6c14844333f7e1af175ea2a533a00627e6
Adarsh singh <adarshsingh458868@gmail.com>
2025-03-26 16:52:06 +0530

    Comment added

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-
+// Serves static files (like index.html, CSS, JS) from the "public" folder.
 app.use(express.static(path.resolve("./public")));
+// Creates a new WebSocket server using Socket.io and attaches it to server.
 const io = new Server(server);
 
 //Socket io
+// Listens for a new client connection.
+
+// Logs socket ID when a user connects.
+// Listens for "user-send-message" event from the client.
+
+// Logs the received message.
+
+// Broadcasts the message to all connected clients (io.emit("message", message)).
+
 
 io.on("connection", (socket) => {
   console.log("user connected", socket.id);
